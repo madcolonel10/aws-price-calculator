98ca32811fe50986eb45b2cc15776ce651148139
Virat Sardana <visardan@cisco.com>
2021-03-26 17:42:33 +0530

    pricing api call working

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/aws/aws-sdk-go-v2 v1.3.0
 	github.com/aws/aws-sdk-go-v2/config v1.1.3
+	github.com/aws/aws-sdk-go-v2/service/pricing v1.2.0
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.3.0
 	github.com/fasthttp/router v1.3.9
 	github.com/valyala/fasthttp v1.22.0
--- main.go
+++ main.go
@@ -1,36 +1,26 @@
 package main
 
-// import (
-// 	"context"
-// 	"fmt"
-// 	"log"
+import (
+	"context"
+	"fmt"
+	"log"
 
-// 	"github.com/aws/aws-sdk-go-v2/aws"
-// 	"github.com/aws/aws-sdk-go-v2/config"
-// 	"github.com/aws/aws-sdk-go-v2/service/s3"
-// )
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/pricing"
+)
 
-// func main() {
-// 	// Load the Shared AWS Configuration (~/.aws/config)
-// 	cfg, err := config.LoadDefaultConfig(context.TODO())
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	// Create an Amazon S3 service client
-// 	client := s3.NewFromConfig(cfg)
-
-// 	// Get the first page of results for ListObjectsV2 for a bucket
-// 	output, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
-// 		Bucket: aws.String("myawsbucketvirat"),
-// 	})
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	log.Println("first page results:")
-// 	for _, object := range output.Contents {
-// 		log.Printf("key=%s size=%d", aws.ToString(object.Key), object.Size)
-// 	}
-// 	fmt.Println("done")
-// }
+func main() {
+	// Load the Shared AWS Configuration (~/.aws/config)
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := pricing.NewFromConfig(cfg)
+	desribeServicesOutput, err := client.DescribeServices(context.TODO(), nil)
+	if err != nil {
+		fmt.Printf("error: %s\n", err)
+		return
+	}
+	fmt.Println(desribeServicesOutput.Services)
+	fmt.Println("done")
+}
